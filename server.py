030464f25c57149ca3b34544b38fd6e9f2af33cc
Ubuntu <ubuntu@ip-172-31-17-68.us-west-2.compute.internal>
2017-11-16 20:30:01 +0000

    Add Flask REST API boilerplate

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 """ server.py: top level module for the Flask application """
 
 from flask import Flask, render_template
+from flask_restful import Resource, Api
+
 app = Flask(__name__)
+api = Api(app) 
 
 @app.errorhandler(404)
 def page_not_found(err):
@@ -14,6 +17,28 @@ def application_error(err):
 @app.route('/')
 def landing():
 	return 'Hello CS496'	
+
+""" REST API ROUTERS """
+class Houses(Resource):
+	def get(self):
+		return 'Hello GET houses'
+
+	def delete(self):
+		return 'Hello DELETE houses'
+
+class Home(Resource):
+	def get(self):
+		return 'Hello GET home'
+	
+	def put(self):
+		return 'Hello PUT home'
+
+	def delete(self):
+		return 'Hello DELETE home'
+	
+
+api.add_resource(Houses, '/houses')
+api.add_resource(Home, '/home')
 
 if __name__ == "__main__":
 	app.run(debug=True)
